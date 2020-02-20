f78e23ef7babff5fb747a9656c01463daf4ad870
Glitch (tangible-romano-5jlnm3k5l) <none>
2020-02-20 19:00:56 +0000

    🎹🕋 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), function(req, res, next){
+  // response
+  console.log(req.file)
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  next();
+})
